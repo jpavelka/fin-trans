8caf472cb73039c99483834bb7d07f0542c84e5e
jpavelka <jeffp171@gmail.com>
2021-06-17 14:07:38 +0000

    format with prettier

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import Login from './auth/Login';
-import Home from './Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { AuthProvider } from './auth/Auth';
+import Login from "./auth/Login";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./auth/Auth";
 
 const App = () => {
   return (
-      <AuthProvider>
-          <Router>
-              {/* <ScrollToTop /> */}
-              {/* <Menu /> */}
-              <Switch>
-                  <Route exact path='/' component={Home} />
-                  <Route exact path='/login' component={Login} />
-              </Switch>
-          </Router>
-      </AuthProvider>
-  )
-}
-
-
-// function App() {
-//   // const txData = useContext(AuthContext);
-//   // console.log(txData);
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+    <AuthProvider>
+      <Router>
+        {/* <ScrollToTop /> */}
+        {/* <Menu /> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+    </AuthProvider>
+  );
+};
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,33 @@
-import { useContext } from 'react';
-import { AuthContext } from './auth/Auth';
-import { app } from './firebase';
+import { useContext } from "react";
+import { AuthContext } from "./auth/Auth";
+import { app } from "./firebase";
 
 const Home = ({ history }) => {
-    const { currentUser, setCurrentUser, txData } = useContext(AuthContext);
-    if (!!!currentUser){
-        history.push('/login');
+  const { currentUser, setCurrentUser, txData } = useContext(AuthContext);
+  if (!!!currentUser) {
+    history.push("/login");
+  }
+  const signOutFunc = () => {
+    const confirmed = window.confirm("Are you sure you want to sign out?");
+    if (!confirmed) {
+      return;
     }
-    const signOutFunc = () => {
-        const confirmed = window.confirm('Are you sure you want to sign out?');
-        if (!confirmed){
-            return
-        }
-        app.auth().signOut();
-        setCurrentUser();
-        history.push('/login');
-    }
-    return (
-        <>
-            <button onClick={signOutFunc}>Sign Out</button>
-            <div>{JSON.stringify(Object.keys(txData || {}).filter(x => txData[x].length > 0).sort())}</div>
-        </>
-    )
-}
+    app.auth().signOut();
+    setCurrentUser();
+    history.push("/login");
+  };
+  return (
+    <>
+      <button onClick={signOutFunc}>Sign Out</button>
+      <div>
+        {JSON.stringify(
+          Object.keys(txData || {})
+            .filter((x) => txData[x].length > 0)
+            .sort()
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
