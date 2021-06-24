cae68be67d4df4462bc90d9002187b8834369e52
jpavelka <jeffp171@gmail.com>
2021-06-24 16:37:07 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Login from "./auth/Login";
 import Home from "./Home";
 import LoadData from "./loadData/LoadData";
+import NavMenu from "./nav/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "./auth/Auth";
 
@@ -8,11 +9,13 @@ const App = () => {
   return (
     <AuthProvider>
       <Router>
-        {/* <Menu /> */}
+        <NavMenu />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/upload" component={LoadData} />
+          <div style={{margin: '5pt'}}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/upload" component={LoadData} />
+          </div>
         </Switch>
       </Router>
     </AuthProvider>
